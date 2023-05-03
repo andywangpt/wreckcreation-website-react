24598b3e311d3ed3065806eba02876367f2207fd
Andy Wang <andywangpt@gmail.com>
2023-05-02 17:27:21 -0700

    deleted button

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,15 +6,6 @@ function NavBar() {
 	return (
 		<>
 			<nav className="navbar navbar-dark navbar-expand-sm py-0">
-
-				<button
-					className="navbar-toggler"
-					data-toggle="collapse"
-					data-target="#navbarCollapse"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-
 				<div className="navbar-collapse collapse" id="navbarCollapse">
 					<ul className="nav navbar-nav mr-auto text-uppercase">
 						<li className="navbar-item m-1">
